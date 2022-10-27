cd5a2498351de55f25614d15e83cb8095595380d
AriyaUnni <ariyaunni2000@gmail.com>
2022-10-27 10:30:05 +0530

    first commit

---
+++ src/main/java/com/nestdigital/companybackendapp/CompanyBackendAppApplication.java
@@ -0,0 +1,13 @@
+package com.nestdigital.companybackendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyBackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyBackendAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestdigital/companybackendapp/CompanyBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.companybackendapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
